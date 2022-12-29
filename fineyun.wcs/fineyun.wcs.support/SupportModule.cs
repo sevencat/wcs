e548888eb87a0cbe454c1a655c125d20fa5e26d1
king <bastet.wang@aliyun.com>
2022-12-29 17:12:47 +0800

    明天来开始写立库的实际代码

--- fineyun.wcs/fineyun.wcs.support/SupportModule.cs
+++ fineyun.wcs/fineyun.wcs.support/SupportModule.cs
@@ -1,12 +1,41 @@
 ﻿using Autofac;
 using fineyun.wcs.common;
+using fineyun.wcs.common.ext;
+using Microsoft.Extensions.Configuration;
 
 namespace fineyun.wcs.support;
 
 public class SupportModule : Module
 {
+	private static readonly NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
+
 	protected override void Load(ContainerBuilder builder)
 	{
 		builder.RegisterType<BeanFactory>().AsSelf().As<IBeanFactory>().SingleInstance();
+		builder.Register((IConfiguration config) => CreateDb(config)).SingleInstance();
+	}
+
+	public IFreeSql CreateDb(IConfiguration config)
+	{
+		var dbconnstr = config["app:db:connstr"];
+		if (string.IsNullOrWhiteSpace(dbconnstr))
+		{
+			Log.Info("未配置数据库");
+			throw new Exception("数据库未配置:app:db:connstr");
+		}
+
+		Log.Info("开始创建数据库:{0}", dbconnstr);
+		var fsql = new FreeSql.FreeSqlBuilder()
+			.UseConnectionString(FreeSql.DataType.MySql, dbconnstr)
+			.UseAutoSyncStructure(false)
+			.Build();
+		var dbshow_sql = config["app:db:showsql"].ToInt32OrDefault(0);
+		if (dbshow_sql == 1)
+		{
+			Log.Info("已开启fsql调试日志");
+			fsql.Aop.CurdBefore += (_, e) => { Log.Info(e.Sql); };
+		}
+
+		return fsql;
 	}
 }
